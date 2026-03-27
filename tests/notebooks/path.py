0b29f70a27463494d52f6d7d1ebd8f009f554b6f
Wojtek Politarczyk <wpolitarczyk@mimuw.edu.pl>
2026-03-27 12:14:17 +0100

    refactor: transition to standard Python 'src' layout

---
+++ tests/notebooks/path.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+# Path to the 'src' directory relative to this file (tests/notebooks/path.py)
+# os.path.dirname(__file__) is tests/notebooks/
+# we go up two levels to get to the root (.. / ..)
+# then we add 'src'
+module_path = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'src'))
+if module_path not in sys.path:
+    sys.path.append(module_path)
